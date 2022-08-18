57293a6da956493769385350256267ff99b16bb0
animelike17 <110774126+animelike17@users.noreply.github.com>
2022-08-18 14:36:30 +0600

    Delete delete.py

--- delete.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def delete_file(file):
-    if os.path.isfile(f"{file}"):
-        os.remove(f"{file}")
-        print(f"File {file} deleted!!!")
-    else:
-        print(f"File not found!”")
-
